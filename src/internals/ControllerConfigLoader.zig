ce1ca5219797c5ee05b1f25632690e14051dcbc8
Antoine Balaine <abalaineaccts@gmail.com>
2024-06-07 19:23:49 -0400

    ControllerConfig: update allocations
    fs_helpers: setup readFile()

--- src/internals/ControllerConfigLoader.zig
+++ src/internals/ControllerConfigLoader.zig
@@ -6,67 +6,75 @@ const reaper = @import("../reaper.zig").reaper;
 const fs_helpers = @import("fs_helpers.zig");
 const Allocator = std.mem.Allocator;
 
-fn readConfig(allocator: Allocator, path: []const u8) !std.json.Parsed(ControllerConfig) {
+fn parseJSON(allocator: Allocator, path: []const u8) !std.json.Parsed(ControllerConfig) {
     const data = try std.fs.cwd().readFileAlloc(allocator, path, 512);
     defer allocator.free(data);
     return std.json.parseFromSlice(ControllerConfig, allocator, data, .{ .allocate = .alloc_always });
 }
 
-fn validateFiles(allocator: *Allocator, controller_name: [*:0]const u8) ![3][*:0]const u8 {
-    const path = fs_helpers.getControllerConfigPath(allocator, controller_name);
-    defer allocator.free(path);
+fn buildPaths(allocator: *Allocator, controller_name: [*:0]const u8) ![3][*:0]const u8 {
+    const controllerConfigDirectory = fs_helpers.getControllerConfigPath(allocator, controller_name);
+    defer allocator.free(controllerConfigDirectory);
     const rfx_extension = ".RfxChain";
 
-    const channelStripPath = try std.fmt.allocPrint(allocator, "prknCtrl_{s}_channelStrip{s}", .{ controller_name, rfx_extension });
+    // read the channelStripPath inside the controllerConfigDirectory
+    // is it possible to do `controller_name ++ "_channelStrip" ++ rfx_extension;`?
+    const chanStripFname = try std.fmt.allocPrint(allocator, "{s}_channelStrip{s}", .{ controller_name, rfx_extension });
+    defer allocator.free(chanStripFname);
+    const chanStripPath = try std.fs.path.join(allocator, &[_][*:0]const u8{ controllerConfigDirectory, chanStripFname });
+    const realearnFname = try std.fmt.allocPrint(allocator, "{s}_realearn{s}", .{ controller_name, rfx_extension });
+    defer allocator.free(realearnFname);
+    const realearnPath = try std.fs.path.join(allocator, &[_][*:0]const u8{ controllerConfigDirectory, realearnFname });
+    const configFname = try std.fmt.allocPrint(allocator, "{s}_config.json", .{controller_name});
+    defer allocator.free(configFname);
+    const configPath = try std.fs.path.join(allocator, &[_][*:0]const u8{ controllerConfigDirectory, configFname });
+    return [_][*:0]const u8{ chanStripPath, realearnPath, configPath };
+}
 
-    const realearnPath = try std.fmt.allocPrint(allocator, "prknCtrl_{s}_realearn{s}", .{ controller_name, rfx_extension });
-    const configPath = try std.fmt.allocPrint(allocator, "prknCtrl_{s}_config.json", .{controller_name});
-    if (!fs_helpers.file_exists(channelStripPath) or !fs_helpers.file_exists(realearnPath) or !fs_helpers.file_exists(configPath)) {
+const Config_FilePaths = std.meta.Tuple(&.{ std.json.Parsed(ControllerConfig), [*:0]const u8, [*:0]const u8, [*:0]const u8 });
+
+fn validateConfig(allocator: *Allocator, controller_name: [*:0]const u8) !Config_FilePaths {
+    const rv = try buildPaths(allocator, controller_name);
+    const channelStripPath = rv[0];
+    const realearnPath = rv[1];
+    const configPath = rv[3];
+    errdefer {
+        allocator.free(channelStripPath);
+        allocator.free(realearnPath);
+        allocator.free(configPath);
+    }
+
+    // check that file paths exist
+    _ = try std.fs.accessAbsolute(channelStripPath, .{}) and std.fs.accessAbsolute(realearnPath, .{}) catch |err| {
         const msg =
-            \\Missing files for controller: 
+            \\Error accessing files for controller: 
             \\channel strip fxchain, 
             \\realearn fxchain,  
-            \\config file 
+            \\ config file 
         ;
 
         var buf: [msg.len + controller_name.len]u8 = undefined;
         _ = try std.fmt.bufPrint(&buf, "{s} {s}", .{ msg, .controller_name });
         reaper.MB(buf, "Error", 0);
-        return ConfigLoaderError.FileUnfound;
-    }
-    return [_][*:0]const u8{ channelStripPath, realearnPath, configPath };
+        return err;
+    };
+
+    // read the config
+    const config = parseJSON(configPath) catch |err| {
+        const msg = "Invalid config file for controller";
+        var buf: [msg.len + controller_name.len + 1]u8 = undefined;
+        _ = try std.fmt.bufPrint(&buf, "{s} {s}", .{ .msg, .controller_name });
+        reaper.MB(buf, "Error", 0);
+        return err;
+    };
+    const retval: Config_FilePaths = .{ config, channelStripPath, realearnPath, configPath };
+    return retval;
 }
 
 const ConfigLoaderError = error{
     NoConfigName,
     FileUnfound,
 };
-
-///@param controller_name string
-///@return ControllerConfig|nil
-fn validateConfig(allocator: *Allocator, controller_name: [*:0]const u8) ConfigLoaderError!ControllerConfig {
-    const rv = try validateFiles(allocator, controller_name);
-    const channelStripPath = rv[0];
-    const realearnPath = rv[1];
-    const configPath = rv[3];
-    defer allocator.free(channelStripPath);
-    defer allocator.free(realearnPath);
-    defer allocator.free(configPath);
-
-    const config = try readConfig(configPath) catch |err| {
-        const msg = "Invalid config file for controller";
-        var buf: [msg.len + controller_name.len]u8 = undefined;
-        _ = try std.fmt.bufPrintZ(&buf, "{s} {s}", .{ .msg, .controller_name });
-        reaper.MB(buf, "Error", 0);
-        return err;
-    };
-    defer config.deinit();
-
-    // TODO make check for contents of data
-    config.channelStripPath = channelStripPath;
-    config.realearnPath = realearnPath;
-    return config;
-}
 
 ///@param controller ControllerId
 ///@return ControllerConfig|nil config
--- src/internals/fs_helpers.zig
+++ src/internals/fs_helpers.zig
@@ -2,11 +2,29 @@ const std = @import("std");
 const reaper = @import("../reaper.zig").reaper;
 const Allocator = std.mem.Allocator;
 
+/// caller must free
 pub fn getControllerConfigPath(allocator: *Allocator, controller_name: [*:0]const u8) ![]const u8 {
     const resourcePath = reaper.GetResourcePath();
-    const file_name = try std.fmt.allocPrint(allocator, "{s}.json", .{controller_name});
-    defer allocator.free(file_name);
     var paths = [_][*:0]const u8{ resourcePath, "Data", "Perken", "Controllers", controller_name };
     const file_path = try std.fs.path.join(allocator, &paths);
     return file_path;
+}
+
+/// caller must free
+/// get length of file, allocate a buffer on length, and read into it.
+pub fn readFile(allocator: Allocator, path: []const u8) ![]u8 {
+
+    // Get the path
+    var path_buffer: [std.fs.max_path_bytes]u8 = undefined;
+    const abs_path = try std.fs.realpath(path, &path_buffer);
+
+    // Open the file
+    const file = try std.fs.openFileAbsolute(abs_path, .{});
+    defer file.close();
+    // get length of file and read to end
+    const len = try file.getEndPos();
+    const buffer = try allocator.alloc(u8, len);
+    const read_len = try file.readAll(buffer);
+    _ = read_len;
+    return buffer;
 }
