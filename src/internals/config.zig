977974bb2756bdb506a913b853c8c15a59baa4f2
Antoine Balaine <abalaineaccts@gmail.com>
2024-07-05 18:49:38 -0400

    config: fix tests (has mem leak)

--- src/internals/config.zig
+++ src/internals/config.zig
@@ -11,11 +11,53 @@ pub const ModulesList = enum {
 };
 
 const Conf = struct {
+    // TODO: switch to StringHashMapUnmanaged
     modules: std.EnumArray(ModulesList, std.StringHashMap(void)),
     defaults: std.EnumArray(ModulesList, []const u8),
+    pub fn init(allocator: std.mem.Allocator) Conf {
+        const self: Conf = .{
+            .modules = std.EnumArray(ModulesList, std.StringHashMap(void)).init(.{
+                .INPUT = std.StringHashMap(void).init(allocator),
+                .GATE = std.StringHashMap(void).init(allocator),
+                .EQ = std.StringHashMap(void).init(allocator),
+                .COMP = std.StringHashMap(void).init(allocator),
+                .SAT = std.StringHashMap(void).init(allocator),
+            }),
+            .defaults = std.EnumArray(ModulesList, []const u8).initUndefined(),
+        };
+        return self;
+    }
+
+    pub fn deinit(self: *Conf, allocator: std.mem.Allocator) void {
+        // FIXME: This causes a mem leak. I can't figure out why.
+        inline for (std.meta.fields(@TypeOf(self.*))) |field| {
+            const V = @field(self, field.name);
+            switch (field.type) {
+                std.EnumArray(ModulesList, std.StringHashMap(void)) => {
+                    // free the keys of the set
+                    inline for (std.meta.fields(ModulesList)) |f| {
+                        var map = V.get(std.meta.stringToEnum(ModulesList, f.name).?);
+                        var keyIter = map.keyIterator();
+                        while (keyIter.next()) |key| {
+                            allocator.free(key.*);
+                        }
+                        map.deinit();
+                    }
+                },
+                std.EnumArray(ModulesList, []const u8) => {
+                    inline for (std.meta.fields(ModulesList)) |f| {
+                        const val = V.get(std.meta.stringToEnum(ModulesList, f.name).?);
+                        allocator.free(val);
+                    }
+                },
+                else => unreachable,
+            }
+        }
+    }
 };
 
 pub fn readConf(allocator: std.mem.Allocator) !Conf {
+    var conf = Conf.init(allocator);
     var mem: [std.fs.MAX_PATH_BYTES]u8 = undefined;
     const pth = try std.fs.cwd().realpath(".", &mem);
 
@@ -27,8 +69,7 @@ pub fn readConf(allocator: std.mem.Allocator) !Conf {
     var defaultsParser = ini.parse(allocator, defaultsFile.reader());
     defer defaultsParser.deinit();
 
-    var defaults = std.EnumArray(ModulesList, []const u8).initUndefined();
-    try ini.readToEnumArray(&defaults, ModulesList, &defaultsParser, allocator);
+    try ini.readToEnumArray(&conf.defaults, ModulesList, &defaultsParser, allocator);
 
     const modulesPath = try std.fs.path.resolve(allocator, &.{ pth, "./resources/modules.ini" });
     defer allocator.free(modulesPath);
@@ -38,26 +79,19 @@ pub fn readConf(allocator: std.mem.Allocator) !Conf {
     var modulesParser = ini.parse(allocator, modulesFile.reader());
     defer modulesParser.deinit();
 
-    // TODO: switch to StringHashMapUnmanaged
-    var modules = std.EnumArray(ModulesList, std.StringHashMap(void)).init(.{
-        .INPUT = std.StringHashMap(void).init(allocator),
-        .GATE = std.StringHashMap(void).init(allocator),
-        .EQ = std.StringHashMap(void).init(allocator),
-        .COMP = std.StringHashMap(void).init(allocator),
-        .SAT = std.StringHashMap(void).init(allocator),
-    });
+    try ini.readToEnumArray(&conf.modules, ModulesList, &modulesParser, allocator);
 
-    try ini.readToEnumArray(&modules, ModulesList, &modulesParser, allocator);
-
-    return Conf{ .defaults = defaults, .modules = modules };
+    return conf;
 }
 
 test readConf {
     const allocator = std.testing.allocator;
     const expect = std.testing.expect;
-    const conf = try readConf(allocator);
+    var conf = try readConf(allocator);
 
     defer {
+
+        // FIXME: this code frees fine, but reproducing it in the deinit method causes a memleak
         inline for (std.meta.fields(@TypeOf(conf))) |field| {
             const V = @field(conf, field.name);
             switch (field.type) {
@@ -81,6 +115,7 @@ test readConf {
                 else => unreachable,
             }
         }
+        // conf.deinit(allocator);
     }
 
     // test defaults
@@ -91,7 +126,5 @@ test readConf {
     // try expect(std.mem.eql(u8, conf.defaults.get(.GATE), "VST: ReaGate (Cockos)"));
     // try expect(std.mem.eql(u8, conf.defaults.get(.SAT), "JS: Saturator"));
 
-    const t = conf.modules.get(.INPUT);
-    const h = t.get("JS: Volume/Pan Smoother");
-    try expect(h != null);
+    try expect(conf.modules.get(.INPUT).get("JS: Volume/Pan Smoother") != null);
 }
