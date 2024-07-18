67c71f8ad3baf8213b0c06362602f18fe72e2da8
Antoine Balaine <abalaineaccts@gmail.com>
2024-07-18 15:54:29 -0400

    build: run gcc build for linux

--- build.zig
+++ build.zig
@@ -17,26 +17,32 @@ pub fn build(b: *std.Build) !void {
     const root = b.path("./src/");
     lib.addIncludePath(root);
 
-    if (target.result.isDarwin()) {
-        lib.root_module.linkFramework("AppKit", .{});
-    }
-    const sourcefileOpts = std.Build.Module.AddCSourceFilesOptions{
-        .files = &.{
-            "src/csurf/control_surface.cpp",
-            "src/csurf/control_surface_wrapper.cpp",
-            // "WDL/swell/swell-modstub-generic.cpp",
-            "WDL/swell/swell-modstub.mm",
-        },
-        .flags = &.{ "-fPIC", "-O2", "-std=c++14", "-IWDL/WDL", "-DSWELL_PROVIDED_BY_APP" },
-    };
-
-    lib.addCSourceFiles(sourcefileOpts);
-    lib.linkLibC();
-    lib.linkLibCpp();
     var client_install: *std.Build.Step.InstallArtifact = undefined;
     if (target.result.isDarwin()) {
+        lib.root_module.linkFramework("AppKit", .{});
+        const sourcefileOpts = std.Build.Module.AddCSourceFilesOptions{
+            .files = &.{
+                "src/csurf/control_surface.cpp",
+                "src/csurf/control_surface_wrapper.cpp",
+                "WDL/swell/swell-modstub.mm",
+            },
+            .flags = &.{ "-fPIC", "-O2", "-std=c++14", "-IWDL/WDL", "-DSWELL_PROVIDED_BY_APP" },
+        };
+
+        lib.addCSourceFiles(sourcefileOpts);
+        lib.linkLibC();
+        lib.linkLibCpp();
         client_install = b.addInstallArtifact(lib, .{ .dest_sub_path = "reaper_zig.dylib" });
     } else {
+        // Not yet tested on Windows.
+        // Linux build has to be done by running the cpp build without zigcc,
+        // because SWELL’s "WDL/swell/swell-modstub-generic.cpp" needs stdlibc++ instead of libc++.
+        const cpp_step = b.addSystemCommand(&[_][]const u8{ "gcc", "-o", "zig-out/lib/control_surface.a", "WDL/swell/swell-modstub-generic.cpp", "src/csurf/control_surface.cpp", "src/csurf/control_surface_wrapper.cpp", "-fPIC", "-O2", "-std=c++14", "-shared", "-IWDL/WDL", "-DSWELL_PROVIDED_BY_APP" });
+
+        const cpp_lib = b.path("zig-out/lib/control_surface.a");
+        lib.root_module.addObjectFile(cpp_lib);
+
+        b.getInstallStep().dependOn(&cpp_step.step);
         client_install = b.addInstallArtifact(lib, .{ .dest_sub_path = "reaper_zig.so" });
     }
 
