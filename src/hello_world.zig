12bb39b0d6b303a2bf7aad16756471c248256a4d
Antoine Balaine <abalaineaccts@gmail.com>
2024-07-23 23:33:42 -0400

    state: remove realearn-related functions

--- src/hello_world.zig
+++ src/hello_world.zig
@@ -15,19 +15,15 @@ const config = @import("internals/config.zig");
 
 const plugin_name = "Hello, Zig!";
 var state: State = undefined;
-var action_id: c_int = undefined;
+// var action_id: c_int = undefined;
 var myCsurf: c.C_ControlSurface = undefined;
 
 var gpa_int = std.heap.GeneralPurposeAllocator(.{ .stack_trace_frames = 999, .verbose_log = false }){};
 const gpa = gpa_int.allocator();
 
 /// retrieve user settings
-/// check tha realearn’s installed,
-/// check whether realearn instances are present on fx monitoring
-/// (load them if not)
 /// retrieve the controller config
 /// register the actions for each of the buttons
-/// return the hook command function that
 fn init() !void {
     const userSettings = UserSettings.init(gpa, "c1");
     const controller_dir = try getControllerPath("c1", gpa);
@@ -61,33 +57,24 @@ export fn ReaperPluginEntry(instance: reaper.HINSTANCE, rec: ?*reaper.plugin_inf
     // };
     std.debug.print("init\n", .{});
 
-    // Define the opaque struct to represent IReaperControlSurface
-    // myCsurf = control_surface.init();
-    // if (myCsurf == null) {
-    //     std.debug.print("Failed to create fake csurf\n", .{});
-    //     deinit();
-    //     return 0;
-    // }
-    // _ = reaper.plugin_register("csurf_inst", myCsurf.?);
-
     _ = reaper.plugin_register("csurf", @constCast(@ptrCast(&control_surface.c1_reg)));
 
     // const action = reaper.custom_action_register_t{ .section = 0, .id_str = "REAIMGUI_ZIG", .name = "ReaImGui Zig example" };
     // action_id = reaper.plugin_register("custom_action", @constCast(@ptrCast(&action)));
-    _ = reaper.plugin_register("hookcommand2", @constCast(@ptrCast(&onCommand)));
+    // _ = reaper.plugin_register("hookcommand2", @constCast(@ptrCast(&onCommand)));
 
     return 1;
 }
 
-fn onCommand(sec: *reaper.KbdSectionInfo, command: c_int, val: c_int, val2hw: c_int, relmode: c_int, hwnd: reaper.HWND) callconv(.C) c_char {
-    _ = .{ sec, val, val2hw, relmode, hwnd };
-
-    if (state.hookCommand(command)) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
+// fn onCommand(sec: *reaper.KbdSectionInfo, command: c_int, val: c_int, val2hw: c_int, relmode: c_int, hwnd: reaper.HWND) callconv(.C) c_char {
+//     _ = .{ sec, val, val2hw, relmode, hwnd };
+//
+//     if (state.hookCommand(command)) {
+//         return 1;
+//     } else {
+//         return 0;
+//     }
+// }
 
 test {
     std.testing.refAllDecls(@This());
