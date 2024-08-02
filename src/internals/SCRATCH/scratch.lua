bc47180bb281e9bd7e107be211dfad7fd36f04ad
Antoine Balaine <abalaineaccts@gmail.com>
2024-08-01 22:05:23 -0400

    scratch: reproduce getSetSC

--- src/internals/SCRATCH/scratch.lua
+++ src/internals/SCRATCH/scratch.lua
@@ -1,3 +1,9 @@
+local VSDEBUG =
+	dofile("/home/antoine/.vscode-oss/extensions/antoinebalaine.reascript-docs-0.1.9/debugger/LoadDebug.lua")
+
+local CONTROLLER_NAME = "PRKN_C1"
+local GATE = 1 -- assume gate and comp are always in same place
+local COMP = 2
 --[[
   **Toggle Mapping of Left Aux Input of FX in Container**
 
@@ -6,39 +12,78 @@
   if it has a container with less than 4 channels, set it to 4
   toggle the left aux input of an fx in the container (subidx tbd)
 ]]
-Tr = reaper.GetSelectedTrack(0, 0)
-Fx = 0
-IsOut = 0 -- input = 0, output = 1
-PluginType, InPins, OutPins = reaper.TrackFX_GetIOSize(Tr, Fx)
-
-TrIns = reaper.GetMediaTrackInfo_Value(Tr, "I_NCHAN")
-if InPins < 4 then
-	reaper.SetMediaTrackInfo_Value(Tr, "I_NCHAN", 4)
+local function getSubContainerIdx(subidx, container_idx, mediaTrack)
+	return 0x2000000 + (subidx * (reaper.TrackFX_GetCount(mediaTrack) + 1)) + container_idx
 end
 
----container_nch : number of internal channels for container<br>
----container_nch_in : number of input pins for container<br>
----container_nch_out : number of output pints for container<br>
----container_nch_feedback : number of internal feedback channels enabled in container<br>
+local SC = {
+	OFF = "off",
+	TOSHAPE = "toshape",
+	TOCOMP = "tocomp",
+}
 
-rv, buf = reaper.TrackFX_GetNamedConfigParm(Tr, Fx, "container_nch")
-if buf < 4 then
-	-- create a container with 4 channels - mapping i/o is automatic
-	reaper.TrackFX_SetNamedConfigParm(Tr, Fx, "container_nch", "4")
-	reaper.TrackFX_SetNamedConfigParm(Tr, Fx, "container_nch_in", "4")
+---@enum SCCHANGE
+local SCCHANGE = {
+	TURNON = 1,
+	TURNOFF = 2,
+	TOGGLE = 3,
+}
+
+---@param tr MediaTrack
+---@param subidx integer
+---@param onoff ?SCCHANGE
+local function getSetFxSC(tr, subidx, onoff)
+	local connected = true
+	local channels = { 2, 3 }
+	local isOutput = 0
+	local hi32 = 0
+	for k, channel in ipairs(channels) do
+		local Lo32, Hi32 = reaper.TrackFX_GetPinMappings(tr, subidx, isOutput, channel) --Get current pin
+		local channelMask = 2 ^ channel
+		local isConnected = (Lo32 & channelMask) > 0
+		if onoff ~= nil then
+			if isConnected then
+				if onoff == SCCHANGE.TURNON then
+					connected = isConnected
+				else
+					Lo32 = Lo32 - channelMask
+					local pinSuccess = reaper.TrackFX_SetPinMappings(tr, subidx, isOutput, channel, Lo32, Hi32)
+					if pinSuccess == true then
+						pinSuccess = not isConnected
+					else
+						pinSuccess = isConnected
+					end
+				end
+			else
+				if onoff == SCCHANGE.TURNOFF then
+					connected = isConnected
+				else
+					Lo32 = Lo32 + channelMask
+					local pinSuccess = reaper.TrackFX_SetPinMappings(tr, subidx, isOutput, channel, Lo32, Hi32)
+					if pinSuccess == true then
+						pinSuccess = not isConnected
+					else
+						pinSuccess = isConnected
+					end
+				end
+			end
+		else
+			connected = isConnected
+		end
+	end
 end
 
-FxInPin = 2 -- left aux
-subIdx = 0x20001 -- getContainerSubIdx
-Lo, Hi = reaper.TrackFX_GetPinMappings(Tr, subIdx, IsOut, FxInPin) --Get current pin
-TrChan = 2
-ChanMask = 2 ^ TrChan
-Val = (Lo & ChanMask) > 0
+function main()
+	local tr = reaper.GetSelectedTrack(0, 0)
+	local container_idx = 0
+	-- local container_idx = reaper.TrackFX_GetByName(tr, CONTROLLER_NAME, false)
+	---container_nch : number of internal channels for container<br>
+	---container_nch_in : number of input pins for container<br>
+	---container_nch_out : number of output pints for container<br>
+	---container_nch_feedback : number of internal feedback channels enabled in container<br>
 
-if Val then
-	Lo = Lo - ChanMask -- disconnect channel
-else
-	Lo = Lo + ChanMask -- connect
+	local subidx2, val = reaper.TrackFX_GetNamedConfigParm(tr, container_idx, "container_item.0")
+	local subidx = getSubContainerIdx(GATE, container_idx+1, tr)
+	getSetFxSC(tr, subidx, nil)
 end
-
-reaper.TrackFX_SetPinMappings(Tr, subIdx, IsOut, FxInPin, Lo, Hi)
+main()
