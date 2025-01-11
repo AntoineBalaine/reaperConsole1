26967467c9dedb64db628018f7d875c68d140402
Antoine Balaine <abalaineaccts@gmail.com>
2025-01-11 14:14:43 -0500

    fx_ctrl: fix layout

--- src/fx_ctrl_panel.zig
+++ src/fx_ctrl_panel.zig
@@ -37,37 +37,6 @@ var out_drive: f64 = 0.5; // Out_Drive
 var out_vol: f64 = 0.5; // Out_Vol
 var out_pan: f64 = 0.5; // Out_Pan
 
-var input_knobs = [_]Knob{
-    .{ .id = @tagName(c1.CCs.Inpt_Gain), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Inpt_Gain) },
-    .{ .id = @tagName(c1.CCs.Inpt_HiCut), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Inpt_HiCut) },
-    .{ .id = @tagName(c1.CCs.Inpt_LoCut), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Inpt_LoCut) },
-};
-
-var shape_knobs = [_]Knob{
-    .{ .id = @tagName(c1.CCs.Shp_Gate), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Shp_Gate) },
-    .{ .id = @tagName(c1.CCs.Shp_GateRelease), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Shp_GateRelease) },
-    .{ .id = @tagName(c1.CCs.Shp_hard_gate), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Shp_hard_gate) },
-};
-
-var eq_knobs = [_]Knob{
-    .{ .id = @tagName(c1.CCs.Eq_LoFrq), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Eq_LoFrq) },
-    .{ .id = @tagName(c1.CCs.Eq_LoGain), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Eq_LoGain) },
-    .{ .id = @tagName(c1.CCs.Eq_LoMidFrq), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Eq_LoMidFrq) },
-    .{ .id = @tagName(c1.CCs.Eq_LoMidGain), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Eq_LoMidGain) },
-};
-
-var comp_knobs = [_]Knob{
-    .{ .id = @tagName(c1.CCs.Comp_Thresh), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Comp_Thresh) },
-    .{ .id = @tagName(c1.CCs.Comp_Ratio), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Comp_Ratio) },
-    .{ .id = @tagName(c1.CCs.Comp_Attack), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Comp_Attack) },
-    .{ .id = @tagName(c1.CCs.Comp_Release), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Comp_Release) },
-};
-
-var out_knobs = [_]Knob{
-    .{ .id = @tagName(c1.CCs.Out_Drive), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Out_Drive) },
-    .{ .id = @tagName(c1.CCs.Out_Vol), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Out_Vol) },
-    .{ .id = @tagName(c1.CCs.Out_Pan), .value = 0.5, .radius = knob_size / 2, .pos = ModuleLayout.getKnobPosition(.Out_Pan) },
-};
 pub fn drawFxControlPanel(ctx: imgui.ContextPtr, state: *State) !void {
     _ = &state;
 
@@ -79,14 +48,15 @@ pub fn drawFxControlPanel(ctx: imgui.ContextPtr, state: *State) !void {
 
     // Input Section
     {
+        try imgui.BeginGroup(.{ctx});
+        defer imgui.EndGroup(.{ctx}) catch {};
         try imgui.Text(.{ ctx, "Input" });
 
         // First column
         _ = try Knobs.drawWidget(ctx, &input_gain, "Gain", .{});
+        try imgui.SameLine(.{ctx});
 
         // Second column (aligned right)
-        const cursor_x = try imgui.GetCursorPosX(.{ctx});
-        try imgui.SetCursorPosX(.{ ctx, cursor_x + knob_size + 20.0 }); // Move to second column
         {
             try imgui.BeginGroup(.{ctx});
             defer imgui.EndGroup(.{ctx}) catch {};
@@ -103,14 +73,16 @@ pub fn drawFxControlPanel(ctx: imgui.ContextPtr, state: *State) !void {
 
     // Shape Section
     {
+        try imgui.BeginGroup(.{ctx});
+        defer imgui.EndGroup(.{ctx}) catch {};
         try imgui.Text(.{ ctx, "Shape" });
 
         // First column
         _ = try Knobs.drawWidget(ctx, &shape_gate, "Gate", .{});
 
+        try imgui.SameLine(.{ctx});
+
         // Second column
-        const cursor_x = try imgui.GetCursorPosX(.{ctx});
-        try imgui.SetCursorPosX(.{ ctx, cursor_x + knob_size + 20.0 });
         {
             try imgui.BeginGroup(.{ctx});
             defer imgui.EndGroup(.{ctx}) catch {};
@@ -126,6 +98,8 @@ pub fn drawFxControlPanel(ctx: imgui.ContextPtr, state: *State) !void {
 
     // EQ Section (single row, four knobs)
     {
+        try imgui.BeginGroup(.{ctx});
+        defer imgui.EndGroup(.{ctx}) catch {};
         try imgui.Text(.{ ctx, "EQ" });
         {
             try imgui.BeginGroup(.{ctx});
@@ -147,14 +121,15 @@ pub fn drawFxControlPanel(ctx: imgui.ContextPtr, state: *State) !void {
 
     // Compressor Section
     {
+        try imgui.BeginGroup(.{ctx});
+        defer imgui.EndGroup(.{ctx}) catch {};
         try imgui.Text(.{ ctx, "Compressor" });
 
         // First column
         _ = try Knobs.drawWidget(ctx, &comp_ratio, "Ratio", .{});
+        try imgui.SameLine(.{ctx});
 
         // Second column
-        const cursor_x = try imgui.GetCursorPosX(.{ctx});
-        try imgui.SetCursorPosX(.{ ctx, cursor_x + knob_size + 20.0 });
         {
             try imgui.BeginGroup(.{ctx});
             defer imgui.EndGroup(.{ctx}) catch {};
@@ -171,6 +146,8 @@ pub fn drawFxControlPanel(ctx: imgui.ContextPtr, state: *State) !void {
 
     // Output Section
     {
+        try imgui.BeginGroup(.{ctx});
+        defer imgui.EndGroup(.{ctx}) catch {};
         try imgui.Text(.{ ctx, "Output" });
 
         // First column
@@ -182,10 +159,9 @@ pub fn drawFxControlPanel(ctx: imgui.ContextPtr, state: *State) !void {
                 _ = try Knobs.drawWidget(ctx, &out_pan, "Pan", .{});
             }
         }
+        try imgui.SameLine(.{ctx});
 
         // Second column
-        const cursor_x = try imgui.GetCursorPosX(.{ctx});
-        try imgui.SetCursorPosX(.{ ctx, cursor_x + knob_size + 20.0 });
         _ = try Knobs.drawWidget(ctx, &out_vol, "Volume", .{});
     }
 }
