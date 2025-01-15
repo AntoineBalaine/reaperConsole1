076ab24bd88c935363f2caa4b0374b30703cad18
Antoine Balaine <abalaineaccts@gmail.com>
2025-01-14 21:39:53 -0500

    csurf: move midi event handler to module

--- src/csurf/control_surface.zig
+++ src/csurf/control_surface.zig
@@ -19,6 +19,7 @@ const globals = @import("../globals.zig");
 const constants = @import("../constants.zig");
 const actions = @import("../actions.zig");
 const logger = @import("../logger.zig");
+const midi_events = @import("midi_events.zig");
 
 const CONTROLLER_NAME = @import("../fx_ctrl_state.zig").CONTROLLER_NAME;
 const reaeq = @import("../internals/reaeq.zig");
@@ -219,73 +220,6 @@ fn selTrck(idx: u8) void {
     zOnTrackSelection(new_tr);
 }
 
-pub fn OnMidiEvent(evt: *c.MIDI_event_t) void {
-    // The console only sends cc messages, so we know that the status is always going to be 0xb0,
-    // except when the message is a running status (i.e. the knobs are turned faster).
-    // In the case of running status, we do need to read the status byte to figure out which control is being touched.
-    // 0xb0 0x1f 0x7f 0x0
-    // ^    ^    ^    ^
-    // |    |    |    useless for our purposes
-    // |    |    value
-    // |    cc number
-    // status "cc message"
-    // 0x6b 0x46 0x0 0xdd
-    // ^    ^    ^    ^
-    // |    |    |    I assume this is noise
-    // |    |    empty
-    // |    value
-    // cc number (byte is < 0xb0, so this is running status)
-    const msg = c.MIDI_event_message(evt);
-    const status = msg[0] & 0xf0;
-    const chan = msg[0] & 0x0f;
-    _ = chan;
-    const cc_enum = std.meta.intToEnum(c1.CCs, if (status == 0xb0) msg[1] else msg[0]) catch null;
-    const val = if (status == 0xb0) msg[2] else msg[1];
-
-    if (cc_enum) |cc| {
-        switch (globals.state.current_mode) {
-            .fx_ctrl => {
-                actions.dispatch(&globals.state, .{ .fx_ctrl = .{ .midi_input = .{ .cc = cc, .value = val } } });
-            },
-            .fx_sel => {
-                switch (cc) {
-                    .Out_Vol => {
-                        // Volume knob for scrolling
-                        actions.dispatch(&globals.state, .{ .fx_sel = .{ .scroll = val } });
-                    },
-                    .Out_solo => {
-                        // Solo button for selection
-                        // if (val > 0) { // On button press
-                        //     const mappings = globals.map_store.getMappingsForModule(globals.state.fx_sel.current_category);
-                        //     if (mappings) |m| {
-                        //         var iterator = m.iterator();
-                        //         var i: usize = 0;
-                        //         while (iterator.next()) |entry| : (i += 1) {
-                        //             if (i == globals.state.fx_sel.scroll_position) {
-                        //                 actions.dispatch(&globals.state, .{ .fx_sel = .{ .select_mapped_fx = .{
-                        //                     .fx_name = entry.key_ptr.*,
-                        //                     .module = globals.state.fx_sel.current_category,
-                        //                 } } });
-                        //                 break;
-                        //             }
-                        //         }
-                        //     }
-                        // }
-                    },
-                    else => {},
-                }
-            },
-            .settings => {
-                switch (cc) {
-                    .Tr_tr20 => actions.dispatch(&globals.state, .{ .change_mode = .fx_ctrl }),
-                    else => {},
-                }
-            },
-            else => {},
-        }
-    }
-}
-
 fn GetTypeString() callconv(.C) [*]const u8 {
     return "CONSOLE1";
 }
@@ -319,7 +253,7 @@ export fn zRun() callconv(.C) void {
         const list = c.MidiIn_GetReadBuf(midi_in);
         var l: c_int = 0;
         while (c.MDEvtLs_EnumItems(list, &l)) |evts| : (l += 1) {
-            OnMidiEvent(evts);
+            midi_events.OnMidiEvent(evts);
         }
         // iterCC();
     }
---
+++ src/csurf/midi_events.zig
@@ -0,0 +1,81 @@
+const std = @import("std");
+const c = @cImport({
+    @cDefine("SWELL_PROVIDED_BY_APP", "");
+    @cInclude("csurf/control_surface_wrapper.h");
+    @cInclude("../WDL/swell/swell-types.h");
+    @cInclude("../WDL/swell/swell-functions.h");
+    @cInclude("../WDL/win32_utf8.h");
+    @cInclude("../WDL/wdltypes.h");
+    @cInclude("resource.h");
+    @cInclude("csurf/midi_wrapper.h");
+});
+const c1 = @import("../c1.zig");
+const globals = @import("../globals.zig");
+const actions = @import("../actions.zig");
+
+pub fn OnMidiEvent(evt: *c.MIDI_event_t) void {
+    // The console only sends cc messages, so we know that the status is always going to be 0xb0,
+    // except when the message is a running status (i.e. the knobs are turned faster).
+    // In the case of running status, we do need to read the status byte to figure out which control is being touched.
+    // 0xb0 0x1f 0x7f 0x0
+    // ^    ^    ^    ^
+    // |    |    |    useless for our purposes
+    // |    |    value
+    // |    cc number
+    // status "cc message"
+    // 0x6b 0x46 0x0 0xdd
+    // ^    ^    ^    ^
+    // |    |    |    I assume this is noise
+    // |    |    empty
+    // |    value
+    // cc number (byte is < 0xb0, so this is running status)
+    const msg = c.MIDI_event_message(evt);
+    const status = msg[0] & 0xf0;
+    const chan = msg[0] & 0x0f;
+    _ = chan;
+    const cc_enum = std.meta.intToEnum(c1.CCs, if (status == 0xb0) msg[1] else msg[0]) catch null;
+    const val = if (status == 0xb0) msg[2] else msg[1];
+
+    if (cc_enum) |cc| {
+        switch (globals.state.current_mode) {
+            .fx_ctrl => {
+                actions.dispatch(&globals.state, .{ .fx_ctrl = .{ .midi_input = .{ .cc = cc, .value = val } } });
+            },
+            .fx_sel => {
+                switch (cc) {
+                    .Out_Vol => {
+                        // Volume knob for scrolling
+                        actions.dispatch(&globals.state, .{ .fx_sel = .{ .scroll = val } });
+                    },
+                    .Out_solo => {
+                        // Solo button for selection
+                        // if (val > 0) { // On button press
+                        //     const mappings = globals.map_store.getMappingsForModule(globals.state.fx_sel.current_category);
+                        //     if (mappings) |m| {
+                        //         var iterator = m.iterator();
+                        //         var i: usize = 0;
+                        //         while (iterator.next()) |entry| : (i += 1) {
+                        //             if (i == globals.state.fx_sel.scroll_position) {
+                        //                 actions.dispatch(&globals.state, .{ .fx_sel = .{ .select_mapped_fx = .{
+                        //                     .fx_name = entry.key_ptr.*,
+                        //                     .module = globals.state.fx_sel.current_category,
+                        //                 } } });
+                        //                 break;
+                        //             }
+                        //         }
+                        //     }
+                        // }
+                    },
+                    else => {},
+                }
+            },
+            .settings => {
+                switch (cc) {
+                    .Tr_tr20 => actions.dispatch(&globals.state, .{ .change_mode = .fx_ctrl }),
+                    else => {},
+                }
+            },
+            else => {},
+        }
+    }
+}
